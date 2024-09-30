7e4a77c7d28dea1f061670184db8ff3b365f3fb3
I539173 <rohit.ravichandran@sap.com>
2024-09-30 19:48:03 +0530

    websocket server with a form UI

---
+++ client.js
@@ -0,0 +1,24 @@
+let ws = new WebSocket("ws://127.0.0.1:4000");
+ws.addEventListener("error", function (err) {
+    console.log(err);
+});
+
+ws.addEventListener("message", function (ev) {
+    let h1 = document.querySelector("#response");
+    h1.innerHTML += ev.data + "<br>";
+});
+
+window.addEventListener(
+    "DOMContentLoaded",
+    function () {
+        const submitBtn = document.querySelector("#form");
+        const inputField = document.querySelector("#chat");
+        submitBtn.addEventListener("submit", function (e) {
+            e.preventDefault();
+            let val = inputField.value;
+            ws.send(val);
+            inputField.value = "";
+        });
+    },
+    false
+);
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("node:http");
+const cors = require("cors");
+const websocket = require("ws");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    console.log("In main path");
+    res.send("in main path");
+});
+
+app.get("/ws", (req, res) => {
+    res.send("Websocket API server");
+});
+
+const server = http.createServer(app);
+
+const wss = new websocket.Server({ server });
+wss.on("connection", (ws, req) => {
+    ws.on("message", (msg) => {
+        console.log("message is " + msg);
+        ws.send(msg + " from server");
+    });
+    console.log("connection established");
+
+    ws.send("welcome");
+});
+
+server.listen(4000, () => {
+    console.log("Listening now...");
+});
